7825ae3f52006668a563fbc7633838cefec4893a
soulevil098123 <72075830+soulevil098123@users.noreply.github.com>
2020-11-25 20:21:40 +0800

    dawdawfgafw

--- connect.js
+++ connect.js
@@ -19,20 +19,10 @@ const port = process.env.PORT || 8001;
   }));
   
  app.use(function (req, res, next) {
-
-
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-
     res.setHeader('Access-Control-Allow-Credentials', true);
-
-
     next();
 });
   
