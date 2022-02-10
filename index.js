542c2b01de45f9e8516775978d77a62ae3dd38c7
soulevil098123 <72075830+soulevil098123@users.noreply.github.com>
2022-02-10 19:46:10 +0800

    feb102022 7:46pm

--- index.js
+++ index.js
@@ -30,225 +30,273 @@ const port = process.env.PORT || 8001;
     next();
 });
 
-app.post("/register", async(req,res)=>{
-    let datas = req.body;
-    data.ref("accounts").orderByChild("email").equalTo(datas.email).once("value", async(snapshot)=>{
-      let ch = false;
-      if(snapshot.val()!=null){
-        snapshot.forEach((x)=>{
-          let key = "";
-          key = x;
-          if(x.val().verified){
-            ch = true;
-            res.send({registered: false,
-              message: 'Email is already registered'
-            });
-          }else{
-            data.ref("accounts").child(x.key).remove();
-          }
-        });   
-      }
-      if(!ch){
-        const id = await checkLastKey('accounts');
-        let date = new Date();
-        let endDate = new Date(parseInt(date.getTime())+86400000).toString();
-        datas.id = id;
-        datas.totalspent = 0;
-        datas.verified = false;
-        datas.verificationCode = generateCode();
-        datas.verifyend = endDate;
-        datas.dateCreated = date.toString();
-        let x = data.ref("accounts").push(datas);
-        data.ref(x.key).child("addresses").push({address:datas.address, primary: true}).then(()=>{
-          email(datas.email, "Verification Code for your Eats Online PH account", datas.verificationCode, datas.name).then((x)=>{
-            if(x){
-              res.send({
-                registered: true,
-                message: 'Successfully registered...'
+app.post("/register", async(req,res, err)=>{
+  if(err){
+    res.status(404).send({d:"Error"});
+  }else{
+    try{
+      let datas = req.body;
+      data.ref("accounts").orderByChild("email").equalTo(datas.email).once("value", async(snapshot)=>{
+        let ch = false;
+        if(snapshot.val()!=null){
+          snapshot.forEach((x)=>{
+            let key = "";
+            key = x;
+            if(x.val().verified){
+              ch = true;
+              res.send({registered: false,
+                message: 'Email is already registered'
               });
-            }else{          
-              res.send({
-                registered: false,
-                message: 'Failed to send verification code...'
-              });
+            }else{
+              data.ref("accounts").child(x.key).remove();
             }
-          })
-        })
-      }
-    })
-});
-
-
-app.post("/login", (req, res)=>{
-  let datas = req.body;
-  data.ref("accounts").orderByChild("email").equalTo(datas.email).once("value", (snapshot)=>{
-    snapshot.forEach((x)=>{
-      if(x.val().password === datas.password){
-        if(x.val().verified){
-          res.send({
-            login: true,
-            message: "Successful"
-          })
-        }else{
-          res.send({
-            login: false,
-            message: "Not verified"
+          });   
+        }
+        if(!ch){
+          const id = await checkLastKey('accounts');
+          let date = new Date();
+          let endDate = new Date(parseInt(date.getTime())+86400000).toString();
+          datas.id = id;
+          datas.totalspent = 0;
+          datas.verified = false;
+          datas.verificationCode = generateCode();
+          datas.verifyend = endDate;
+          datas.dateCreated = date.toString();
+          let x = data.ref("accounts").push(datas);
+          data.ref(x.key).child("addresses").push({address:datas.address, primary: true}).then(()=>{
+            email(datas.email, "Verification Code for your Eats Online PH account", datas.verificationCode, datas.name).then((x)=>{
+              if(x){
+                res.send({
+                  registered: true,
+                  message: 'Successfully registered...'
+                });
+              }else{          
+                res.send({
+                  registered: false,
+                  message: 'Failed to send verification code...'
+                });
+              }
+            })
           })
         }
-      }else{
-        res.send({
-          login: false,
-          message: "Wrong password"
-        });
-      }
-    });
-  });
-});
-app.patch("/verify", (req, res)=>{
-  let datas = req.body;
-  data.ref("accounts").orderByKey().equalTo(datas.id).once("value", (snapshot)=>{
-    if(snapshot.val()==null){
-      res.send({
-        verified: false,
-        message: "Account not found"
       })
-    }else{
-      snapshot.forEach((snap)=>{
-        if(!snap.val().verified){
-          if(snap.val().verificationCode===datas.code){
-            data.ref("accounts").child(snap.key).update({verified: true, verificationCode: null, verifyend: null}).then(()=>{
+    }catch(e){
+      res.status(500).send({error: true, message: e.toString()});
+    }
+  }
+})
+
+
+app.post("/login", (req, res, err)=>{
+  if(err){
+    res.status(500).send({error: true, message: "Error"});
+  }else{
+    try{
+      let datas = req.body;
+      data.ref("accounts").orderByChild("email").equalTo(datas.email).once("value", (snapshot)=>{
+        snapshot.forEach((x)=>{
+          if(x.val().password === datas.password){
+            if(x.val().verified){
               res.send({
-                verified: true,
-                message: "Your account has been verified!"
+                id: x.key,
+                login: true,
+                message: "Successful"
               })
-            })
+            }else{
+              res.send({
+                id: x.key,
+                login: false,
+                message: "Not verified"
+              })
+            }
+          }else{
+            res.send({
+              login: false,
+              message: "Wrong password"
+            });
+          }
+        });
+      });
+    }catch(e){
+      res.status(500).send({error: true, message: e.toString()});
+    }
+  }
+});
+app.patch("/verify", (req, res, err)=>{
+  if(err){
+    res.status(500).send({error: true, message: "Error"});
+  }else{
+    try{
+    let datas = req.body;
+    data.ref("accounts").orderByKey().equalTo(datas.id).once("value", (snapshot)=>{
+      if(snapshot.val()==null){
+        res.send({
+          verified: false,
+          message: "Account not found"
+        })
+      }else{
+        snapshot.forEach((snap)=>{
+          if(!snap.val().verified){
+            if(snap.val().verificationCode===datas.code){
+              data.ref("accounts").child(snap.key).update({verified: true, verificationCode: null, verifyend: null}).then(()=>{
+                res.send({
+                  verified: true,
+                  message: "Your account has been verified!"
+                })
+              })
+            }else{
+              res.send({
+                verified: false,
+                message: "Wrong Verification Code!"
+              });
+            }
           }else{
             res.send({
               verified: false,
-              message: "Wrong Verification Code!"
+              message: "Account is already verified!"
             });
           }
-        }else{
-          res.send({
-            verified: false,
-            message: "Account is already verified!"
-          });
-        }
-      })
+        })
+      }
+    })
+    }catch(e){
+      res.status(500).send({error: true, message: e.toString()});
     }
-  })
+  }
 })
 
-app.post("/profileData", (req, res) => {
-  try{
-    let datas = req.body;
-    data.ref("accounts").orderByKey().equalTo(datas.id).once("value", (snapshot)=>{
-      let object = {};  
-      snapshot.forEach((snaps)=>{
-          for(let key in snaps.val()){
-            if(typeof datas.data === "object"){
-              if(datas.data.includes(key)){
-                object[key] = snaps.val()[key];
+app.post("/profileData", (req, res, err) => {
+  if(err){
+      res.status(500).send({error: true, message: "Error"});
+  }else{
+    try{
+      let datas = req.body;
+      data.ref("accounts").orderByKey().equalTo(datas.id).once("value", (snapshot)=>{
+        let object = {};  
+        snapshot.forEach((snaps)=>{
+            for(let key in snaps.val()){
+              if(typeof datas.data === "object"){
+                if(datas.data.includes(key)){
+                  object[key] = snaps.val()[key];
+                }
               }
             }
-          }
-        })
-      res.send(object);
-    });
-  }catch(e){
-    res.send({error:true, errorMessage: e.toString()})
+          })
+        res.send(object);
+      });
+    }catch(e){
+      res.status(500).send({error: true, message: e.toString()});
+    }
   }
 });
 
 
-app.post("/search", (req, res)=>{
-  try{
-    let datas = req.body;
-    data.ref(datas.reference).orderByChild(datas.data).startAt(datas.value.toUpperCase()).endAt(datas.value.toLowerCase()+ "\uf8ff").once("value", (snapshot) =>{
-      let x = [];
-      snapshot.forEach((snap)=>{
-        if(snap.val()[datas.data].toLowerCase().includes(datas.value.toLowerCase())){
-          x.push([snap.key, snap.val()]);
-        }
-      });
-      res.send({
-        search:true,
-        data: x
-      })
-    });
-  }catch(e){
-    res.send({error:true, errorMessage: e.toString()});
-  }
-})
-
-app.post("/getData", (req, res)=>{
-  try{
-    let datas = req.body;
-   
-    data.ref(datas.reference).orderByChild(datas.sortwhat).once("value", (snapshot)=>{
-      let x = [];
-      if("index" in datas){
-        let i = 0;
+app.post("/search", (req, res, err)=>{
+  if(err){
+    res.status(500).send({error: true, message: "Error"});
+  }else{
+    try{
+      let datas = req.body;
+      data.ref(datas.reference).orderByChild(datas.data).startAt(datas.value.toUpperCase()).endAt(datas.value.toLowerCase()+ "\uf8ff").once("value", (snapshot) =>{
+        let x = [];
         snapshot.forEach((snap)=>{
-          if(i >= (snapshot.numChildren()-datas.index[1]) && i <= (snapshot.numChildren() - datas.index[0])){
+          if(snap.val()[datas.data].toLowerCase().includes(datas.value.toLowerCase())){
             x.push([snap.key, snap.val()]);
           }
-          i++;
+        });
+        res.send({
+          search:true,
+          data: x
         })
-      }else{
-        snapshot.forEach((snap)=>{
-          x.push([snap.key, snap.val()]);
+      });
+    }catch(e){
+      res.status(500).send({error: true, message: e.toString()});
+    }
+  }
+})
+
+app.post("/getData", (req, res, err)=>{
+  if(err){
+    res.status(500).send({error: true, message: "Error"});
+  }else{
+    try{
+      let datas = req.body;
+      data.ref(datas.reference).orderByChild(datas.sortwhat).once("value", (snapshot)=>{
+        let x = [];
+        if("index" in datas){
+          let i = 0;
+          snapshot.forEach((snap)=>{
+            if(i >= (snapshot.numChildren()-datas.index[1]) && i <= (snapshot.numChildren() - datas.index[0])){
+              x.push([snap.key, snap.val()]);
+            }
+            i++;
+          })
+        }else{
+          snapshot.forEach((snap)=>{
+            x.push([snap.key, snap.val()]);
+          })
+        }
+        x.reverse();
+        res.send({
+          data: x
         })
-      }
-      x.reverse();
-      res.send({
-        data: x
       })
-    })
-  }catch(e){
-    res.send({error: true, errorMessage: e.toString()});
+    }catch(e){
+      res.status(500).send({error: true, message: e.toString()});
+    }
   }
 })
 
-app.post("/comment", (req, res)=>{
-  try{
-    let datas = req.body;
-    data.ref("products").child(datas.id).child('comments').push({date: new Date().toString(), message: datas.message, user: datas.user, rating: datas.rate, email: datas.email, uid: datas.uid}).then(()=>{
-      res.send({
-        comment: datas.message,
-        success: true,
-        message: "Comment posted!"
-      })
-    });
-  }catch(e){
-    res.send({error: true, errorMessage: e.toString()});
+app.post("/comment", (req, res, err)=>{
+  if(err){
+    res.status(500).send({error: true, message: "Error"});
+  }else{
+    try{
+      let datas = req.body;
+      data.ref("products").child(datas.id).child('comments').push({date: new Date().toString(), message: datas.message, user: datas.user, rating: datas.rate, email: datas.email, uid: datas.uid}).then(()=>{
+        res.send({
+          comment: datas.message,
+          success: true,
+          message: "Comment posted!"
+        })
+      });
+    }catch(e){
+      res.status(500).send({error: true, message: e.toString()});
+    }
   }
 })
 
-app.get("/comment", (req, res) =>{
-  try{
-    let datas = req.body;
-    data.ref("products").child(datas.id).child("comments").once("value", (snapshot)=>{
-      let x = [];
-      snapshot.forEach((val)=>{
-        x.push([val.key, val.val()]);
+app.get("/comment", (req, res, err) =>{
+  if(err){
+    res.status(500).send({error: true, message: "Error"});
+  }else{
+    try{
+      let datas = req.body;
+      data.ref("products").child(datas.id).child("comments").once("value", (snapshot)=>{
+        let x = [];
+        snapshot.forEach((val)=>{
+          x.push([val.key, val.val()]);
+        })
+        res.send({data:x});
       })
-      res.send({data:x});
-    })
-  }catch(e){
-    res.send({error:true, errorMessage: e.toString()});
-  }
-
+    }catch(e){
+      res.status(500).send({error: true, message: e.toString()});
+    }
+    }
 })
 
-app.patch("/profileData", (req, res)=>{
-  try{
-    let datas = req.body;
-    data.ref("accounts").child(datas.id).update(datas.data).then(()=>{
-      res.send({update: true, message:"Account Updated!"});
-    })
-  }catch(e){
-    res.send({error:true, errorMessage: e.toString()})
+app.patch("/profileData", (req, res, err)=>{
+  if(err){
+    res.status(500).send({error: true, message: "Error"});
+  }else{
+    try{
+      let datas = req.body;
+      data.ref("accounts").child(datas.id).update(datas.data).then(()=>{
+        res.send({update: true, message:"Account Updated!"});
+      })
+    }catch(e){
+      res.status(500).send({error: true, message: e.toString()});
+    }
   }
 })
 
